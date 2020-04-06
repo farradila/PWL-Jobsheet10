4e904e122570e8fc52190d5fca52281e4082ee15
farradila <farradila429@gmail.com>
2020-04-06 08:49:53 +0700

    Jobsheet10-Farradila

---
+++ laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil Saya</title>
+</head>
+<body>
+	<h1>Profil Saya</h1>
+	<p>Perkenalkan Nama saya Farradila Ayu Damayanti, Saya seorang Mahasiswa D4 Teknik Informatika di Politeknik Negeri Malang.</p>
+</body>
+</html>
